792525e6b03a501ce95080aee809c20939b5955f
Alexandre Blondeau <deformhead@gmail.com>
2018-12-08 10:39:52 +0100

    update 'framerate' and add 'speed'

--- sources/loop.js
+++ sources/loop.js
@@ -1,16 +1,22 @@
-function Loop(framerate) {
-
-    const timeframe = 1000 / framerate;
+function Loop(framerate = 60, speed = 1) {
 
     let elapsedTime = 0;
+    let lastRender = null;
     let lastUpdate = null;
 
     function render(handler) {
 
-        handler();
+        const currentRender = Date.now();
+
+        if (lastRender !== null) {
+
+            handler(currentRender - lastRender);
+        }
 
         // call user's render handler on each available frame
         requestAnimationFrame(this.render.bind(this, handler));
+
+        lastRender = currentRender;
     }
 
     function update(handler) {
@@ -23,20 +29,23 @@ function Loop(framerate) {
             elapsedTime += currentUpdate - lastUpdate;
         }
 
-        // call user's update handler matching timeframe and fixing browser time handling
-        while (elapsedTime >= timeframe) {
+        // call user's update handler matching timeframe, speed and fixing browser time handling
+        while (elapsedTime >= 1000 / this.framerate / this.speed) {
 
-            // define elapsed time since last user's update handler matching timeframe
-            elapsedTime -= timeframe;
+            // define elapsed time since last user's update handler matching timeframe and speed
+            elapsedTime -= 1000 / this.framerate / this.speed;
 
-            handler(timeframe);
+            handler(1000 / this.framerate);
         }
 
         lastUpdate = currentUpdate;
 
-        // call user's update handler matching timeframe
-        setTimeout(this.update.bind(this, handler), timeframe);
+        // call update loop at least 60 times per second to quickly catch framerate or speed changes
+        setTimeout(this.update.bind(this, handler), 1000 / Math.max(this.framerate, 60));
     }
+
+    this.framerate = framerate;
+    this.speed = speed;
 
     this.render = render;
     this.update = update;
